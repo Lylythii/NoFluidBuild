c12018dce673f3a8076ae7fe4760e9fe8836c4a0
lothrazar <samson.bassett@gmail.com>
2020-05-25 15:07:45 -0700

    1.12 port looks good now

--- src/main/java/com/lothrazar/nolavabuild/LavaBlockMod.java
+++ src/main/java/com/lothrazar/nolavabuild/LavaBlockMod.java
@@ -1,51 +1,35 @@
 package com.lothrazar.nolavabuild;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import net.minecraft.block.BlockLiquid;
 import net.minecraft.block.state.IBlockState;
-import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.world.BlockEvent;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLFingerprintViolationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
-@Mod(modid = LavaBlockMod.MODID)
+@Mod(modid = LavaBlockMod.MODID, certificateFingerprint = "@FINGERPRINT@")
 public class LavaBlockMod {
 
-  private String certificateFingerprint = "@FINGERPRINT@";
-  //  public static final IProxy proxy = DistExecutor.runForDist(() -> () -> new ClientProxy(), () -> () -> new ServerProxy());
   public static final String MODID = "nolavabuild";
-  private static final Logger LOGGER = LogManager.getLogger();
+  //  private static final Logger LOGGER = LogManager.getLogger();
 
-  public LavaBlockMod() {
+  @EventHandler
+  public void onPreInit(FMLPreInitializationEvent event) {
     MinecraftForge.EVENT_BUS.register(this);
   }
 
   @SubscribeEvent
-  public void onRightClickBlock(BlockEvent.EntityPlaceEvent event) {
-    IBlockState current = event.getBlockSnapshot().getReplacedBlock();
-    if (!event.getWorld().isRemote
-        && event.getEntity() instanceof EntityPlayer
-        && current.getBlock() == Blocks.LAVA) {
-      //      System.out.println("? cancel " + current.getValue(BlockLiquid.LEVEL));
-      if (current.getValue(BlockLiquid.LEVEL) == 0)
-        event.setCanceled(true);
+  public void onRightClickBlock(PlayerInteractEvent.RightClickBlock event) {
+    IBlockState current = event.getWorld().getBlockState(event.getPos().offset(event.getFace()));
+    if ((current.getBlock() == Blocks.LAVA
+        || current.getBlock() == Blocks.FLOWING_LAVA)
+        && current.getValue(BlockLiquid.LEVEL) == 0) {
+      event.setCanceled(true);
     }
-    //    IBlockState current = event.getWorld().getBlockState(
-    //        event.getPos().offset(event.getFace()));
-    //    && current.getFluidState().isSource()
-    //    if (current.getBlock() == Blocks.LAVA) {//  && current.getValue(BlockLiquid.LEVEL) == 0
-    //      System.out.println("? build " + current.getValue(BlockLiquid.LEVEL));
-    //      if (current.getValue(BlockLiquid.LEVEL) == 0) {
-    //        event.setCanceled(true);
-    //        return;
-    //      }
-    //      System.out.println("do not cancel " + current.getValue(BlockLiquid.LEVEL));
-    //    }
-    //    BlockEvent.EntityPlaceEvent
   }
   //  @SubscribeEvent
   //  public void onFluidPlaceBlockEvent(BlockEvent.FluidPlaceBlockEvent event) {
@@ -55,11 +39,11 @@ public class LavaBlockMod {
   //    }
   //  }
 
-  @SubscribeEvent
-  public static void onFingerprintViolation(FMLFingerprintViolationEvent event) {
+  @EventHandler
+  public void onFingerprintViolation(FMLFingerprintViolationEvent event) {
     // https://tutorials.darkhax.net/tutorials/jar_signing/
     String source = (event.getSource() == null) ? "" : event.getSource().getName() + " ";
     String msg = MODID + "Invalid fingerprint detected! The file " + source + "may have been tampered with. This version will NOT be supported by the author!";
-    //    System.out.println(msg);
+    System.out.println(msg);
   }
 }
