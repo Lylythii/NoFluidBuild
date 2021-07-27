9a791d8584e64473889dfd44a7e2f802162b14eb
lothrazar <samson.bassett@gmail.com>
2021-07-26 19:11:42 -0700

    1.17 ported with forge gradle

--- src/main/java/com/lothrazar/nolavabuild/LavaBlockMod.java
+++ src/main/java/com/lothrazar/nolavabuild/LavaBlockMod.java
@@ -1,12 +1,13 @@
 package com.lothrazar.nolavabuild;
 
-import net.minecraft.block.BlockState;
-import net.minecraft.block.Blocks;
+import net.minecraft.world.entity.item.FallingBlockEntity;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import net.minecraftforge.event.world.BlockEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLFingerprintViolationEvent;
 
 @Mod(LavaBlockMod.MODID)
 public class LavaBlockMod {
@@ -14,34 +15,15 @@ public class LavaBlockMod {
   public static final String MODID = "nolavabuild";
 
   public LavaBlockMod() {
-    //only for server starting
     MinecraftForge.EVENT_BUS.register(this);
-    //    ConfigHandler.loadConfig(ConfigHandler.COMMON_CONFIG, FMLPaths.CONFIGDIR.get().resolve(MODID + ".toml"));
   }
 
   @SubscribeEvent
   public void onRightClickBlock(PlayerInteractEvent.RightClickBlock event) {
-    BlockState current = event.getWorld().getBlockState(
-        event.getPos().offset(event.getFace()));
+    BlockState current = event.getWorld().getBlockState(event.getPos().relative(event.getFace()));
     if (current.getBlock() == Blocks.LAVA
         && current.getFluidState().isSource()) {
       event.setCanceled(true);
     }
-  }
-  //  @SubscribeEvent
-  //  public void onFluidPlaceBlockEvent(BlockEvent.EntityPlaceEvent event) {
-  //    System.out.println("BlockEvent. bbb  " + event.getPlacedBlock());
-  //    System.out.println("BlockEvent. eee " + event.getEntity());
-  //    if (event.getEntity() instanceof FallingBlockEntity) {
-  //      event.setCanceled(true);
-  //    }
-  //  }
-
-  @SubscribeEvent
-  public static void onFingerprintViolation(FMLFingerprintViolationEvent event) {
-    // https://tutorials.darkhax.net/tutorials/jar_signing/
-    String source = (event.getSource() == null) ? "" : event.getSource().getName() + " ";
-    String msg = MODID + "Invalid fingerprint detected! The file " + source + "may have been tampered with. This version will NOT be supported by the author!";
-    //    System.out.println(msg);
   }
 }
